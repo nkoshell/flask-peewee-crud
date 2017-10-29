92a148ae99bd70abddf2878afa511105883c8a9e
nkoshell <nikita.koshelev@gmail.com>
2017-10-29 20:00:13 +0300

    Bum to version 0.3.0

--- flask_peewee_crud/__init__.py
+++ flask_peewee_crud/__init__.py
@@ -1,6 +1,6 @@
 from .config import CrudConfig, ResponseMessages
 from .crud_generation import generate_crud
 
-__version__ = '0.1.4'
+__version__ = '0.3.0'
 
 __all__ = ['CrudConfig', 'ResponseMessages', 'generate_crud']
