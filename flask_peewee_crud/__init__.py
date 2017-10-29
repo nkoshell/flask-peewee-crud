382092962345ee7ce197d77e08de054b4bccc878
nkoshell <nikita.koshelev@gmail.com>
2017-10-29 18:18:33 +0300

    Update setup.py

--- flask_peewee_crud/__init__.py
+++ flask_peewee_crud/__init__.py
@@ -1,6 +1,6 @@
 from .config import CrudConfig, ResponseMessages
 from .crud_generation import generate_crud
 
-__version__ = '0.2.4'
+__version__ = '0.1.2'
 
 __all__ = ['CrudConfig', 'ResponseMessages', 'generate_crud']
--- setup.py
+++ setup.py
@@ -1,14 +1,39 @@
+import codecs
+import os
+import re
+
 from setuptools import setup
+
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+VERSION_REGEXP = re.compile(r"^__version__ = [\'\"](.+?)[\'\"]$", re.MULTILINE)
+
+
+def read(fn):
+    with codecs.open(os.path.join(PROJECT_DIR, fn), encoding='utf-8') as f:
+        return f.read().strip()
+
+
+def version():
+    try:
+        return VERSION_REGEXP.findall(read(os.path.join('aio_space_track_api', '__init__.py')))[0]
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
+
+
+vn = version()
+url = 'https://github.com/nkoshell/flask-peewee-crud'
 
 setup(
     name='flask-peewee-crud',
-    version='0.1.1',
-    url='https://github.com/nkoshell/flask-peewee-crud',
+    version=vn,
+    url=url,
     license='MIT',
     author='nkoshell',
     author_email='nikita.koshelev@gmail.com',
     description=('A REST API framework for building CRUD APIs using Flask and peewee '
                  '(forked from sanic_crud https://github.com/Typhon66/sanic_crud)'),
+    long_description=read('README.rst'),
+    download_url='{url}/archive/{version}.tar.gz'.format(url=url, version=vn),
     packages=['flask_peewee_crud', 'flask_peewee_crud.resources'],
     platforms='any',
     install_requires=[
