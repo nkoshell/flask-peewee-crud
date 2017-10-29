b781d887211256bdce9e332812e8b01d14690943
nkoshell <nikita.koshelev@gmail.com>
2017-10-29 19:41:33 +0300

    Some fixes for pypi

--- flask_peewee_crud/__init__.py
+++ flask_peewee_crud/__init__.py
@@ -1,6 +1,6 @@
 from .config import CrudConfig, ResponseMessages
 from .crud_generation import generate_crud
 
-__version__ = '0.1.2'
+__version__ = '0.1.4'
 
 __all__ = ['CrudConfig', 'ResponseMessages', 'generate_crud']
